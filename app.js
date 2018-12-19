36a915e967d6a987cc190a0022b74e2151cd33b4
Yao Liu <yliu233@gwu.edu>
2018-12-19 17:14:13 +0000

    add mlab

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ var commentRoutes = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes = require("./routes/index");
 
-mongoose.connect("mongodb://localhost/yelp_camp");
+//mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://yliu233:ly920616@ds263989.mlab.com:63989/yao-yelpcamp");
+//mongodb://yliu233:ly920616@ds263989.mlab.com:63989/yao-yelpcamp
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
